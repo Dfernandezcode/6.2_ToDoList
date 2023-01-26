e52d98cb925a197aedd79d72178536b820b799d7
Daniel Fernandez <dfernandezcode@gmail.com>
2023-01-26 22:08:11 +0100

    html and css complete
    basic JS added

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-let taskItem;
+//Element Selection
+
+const form = document.getElementById('toDoForm');
+
+// Variables
+let toDos = [];
+
+// Form Submission
+form.addEventListener('submit', function (event) {
+	event.preventDefault();
+	console.log('submit');
+
+	saveToDo();
+});
+// Save TODO function
+function saveToDo() {}
